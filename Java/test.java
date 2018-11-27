dfdb77ba3cb550a2358cbb06ed50396f0c32508e
AdoryVo <33337855+AdoryVo@users.noreply.github.com>
2018-11-26 22:07:46 -0800

    Add files via upload

---
+++ Java/test.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class test
+{
+	public static void main(String[] args)
+	{
+		Random rand = new Random();
+		
+		System.out.println(rand.nextInt(100000000)+100000000);
+	}
+}
